89cb961e25ea0278204df1e88d1e231109d3b334
artem-burashnikov <artem.burashnikov@gmail.com>
2025-05-02 19:56:01 +0300

    feat: add unit tests for Close method

---
+++ pkg/subpub/subpub_test.go
@@ -0,0 +1,76 @@
+package subpub
+
+import (
+	"context"
+	"errors"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestClose(t *testing.T) {
+	bus := New()
+
+	bus.Subscribe("foo", func(msg any) {})
+
+	err := bus.Close(context.Background())
+	assert.NoError(t, err)
+}
+
+func TestCloseWithDeadline(t *testing.T) {
+	bus := New()
+
+	bus.Subscribe("foo", func(msg any) {
+		time.Sleep(1 * time.Second)
+	})
+
+	bus.Publish("foo", "bar")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 100*time.Millisecond)
+	defer cancel()
+
+	start := time.Now()
+	err := bus.Close(ctx)
+	duration := time.Since(start)
+
+	assert.True(t, errors.Is(err, context.DeadlineExceeded), "expected context.DeadlineExceeded, got %v", err)
+	assert.LessOrEqual(t, duration, 200*time.Millisecond, "Close took too long: %v", duration)
+}
+
+func TestSubscribeAfterClose(t *testing.T) {
+	bus := New()
+	bus.Close(context.Background())
+
+	_, err := bus.Subscribe("foo", func(msg any) {})
+	assert.Equal(t, ErrClosed, err)
+}
+
+func TestPublishAfterClose(t *testing.T) {
+	bus := New()
+	bus.Close(context.Background())
+
+	err := bus.Publish("foo", "bar")
+	assert.Equal(t, ErrClosed, err)
+}
+
+func TestDoubleClose(t *testing.T) {
+	bus := New()
+
+	err := bus.Close(context.Background())
+	assert.NoError(t, err)
+
+	err = bus.Close(context.Background())
+	assert.Equal(t, ErrClosed, err)
+}
+
+func TestCloseWithMultipleSubscribers(t *testing.T) {
+	bus := New()
+
+	bus.Subscribe("foo", func(msg any) {})
+	bus.Subscribe("bar", func(msg any) {})
+	bus.Subscribe("baz", func(msg any) {})
+
+	err := bus.Close(context.Background())
+	assert.NoError(t, err)
+}
