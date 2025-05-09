e1efcfbb567727c472aa22f5ae2137a9ac3b7749
artem-burashnikov <artem.burashnikov@gmail.com>
2025-05-09 18:20:22 +0300

    feat: add server entry point

---
+++ cmd/server/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"github.com/artem-burashnikov/grpc-subpub/internal/config"
+	"github.com/artem-burashnikov/grpc-subpub/internal/logger"
+	"github.com/artem-burashnikov/grpc-subpub/internal/server"
+	"github.com/artem-burashnikov/grpc-subpub/pkg/subpub"
+)
+
+func Must[T any](obj T, err error) T {
+	if err != nil {
+		panic(err)
+	}
+	return obj
+}
+
+func main() {
+	const defaultConfigPath = "config.yaml"
+
+	cfg := Must(config.Load(defaultConfigPath))
+
+	log := logger.NewZap(cfg.App.Environment)
+
+	sp := subpub.New()
+
+	s := server.New(cfg, log, sp)
+
+	s.Run()
+}
